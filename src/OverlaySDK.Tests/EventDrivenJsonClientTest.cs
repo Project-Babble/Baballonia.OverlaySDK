954051d1c5943facdc197a09603fbd989e5063ec
RedInJector <imaxprots@gmail.com>
2025-10-02 00:58:28 +0200

    add stuff

---
+++ src/OverlaySDK.Tests/EventDrivenJsonClientTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.Json;
+using OverlaySDK;
+using OverlaySDK.Packets;
+
+namespace OverlaySDK.Tests;
+
+public class EventDrivenJsonClientTest
+{
+    
+    [Fact]
+    public async Task Integration()
+    {
+        var socketFactory = new SocketFactory();
+
+        Packet<RunFixedLenghtRoutinePacket> testPacket =
+            new Packet<RunFixedLenghtRoutinePacket>(new RunFixedLenghtRoutinePacket("ballz"));
+
+        var t1 = Task.Run(() =>
+        {
+            List<JsonDocument> docs = new();
+            var tcp1 = new EventDrivenTcpClient(socketFactory.CreateServer("127.0.0.1", 1234));
+            var json1 = new EventDrivenJsonClient(tcp1);
+            json1.DataReceived += doc =>
+            {
+                docs.Add(doc);
+            };
+            json1.Send(testPacket);
+            json1.Send(testPacket);
+            json1.Send(testPacket);
+
+            Thread.Sleep(30);
+
+            return docs;
+        });
+        
+        var t2 = Task.Run(() =>
+        {
+            List<JsonDocument> docs = new();
+            var tcp2 = new EventDrivenTcpClient(socketFactory.CreateClient("127.0.0.1", 1234));
+            var json2 = new EventDrivenJsonClient(tcp2);
+            json2.DataReceived += doc =>
+            {
+                docs.Add(doc);
+            };
+
+            json2.Send(testPacket);
+            json2.Send(testPacket);
+
+            Thread.Sleep(30);
+
+            return docs;
+        });
+
+        var res1 = await t1;
+        var res2 = await t2;
+
+        Assert.Equal(2, res1.Count);
+        Assert.Equal(3, res2.Count);
+
+        Assert.Equal(testPacket.PacketName, res1.First().Deserialize<IncomingPacket>()!.PacketName);
+        Assert.Equal(testPacket.PacketName, res2.First().Deserialize<IncomingPacket>()!.PacketName);
+    }
+}
---
+++ src/OverlaySDK.Tests/OverlayMessageDispatcherTest.cs
@@ -0,0 +1,127 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using Moq;
+using OverlaySDK;
+using OverlaySDK.Packets;
+
+namespace OverlaySDK.Tests;
+
+public class OverlayMessageDispatcherTest
+{
+    [Fact]
+    public void DispatchOneSuccess()
+    {
+        ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        LoggerImpl loggerImpl = new LoggerImpl(factory.CreateLogger<OverlayMessageDispatcher>());
+
+        Packet<RunFixedLenghtRoutinePacket> packet =
+            new Packet<RunFixedLenghtRoutinePacket>(new RunFixedLenghtRoutinePacket("balls"));
+        var serializedJson = JsonSerializer.Serialize(packet);
+
+        var mockConnection = new Mock<IEventDrivenConnection<object, JsonDocument>>();
+
+        var overlayDispatcher =
+            new OverlayMessageDispatcher(loggerImpl, mockConnection.Object);
+
+        var mockHandler = new Mock<PacketHandlerAdapter>();
+        mockHandler
+            .Setup(adapter => adapter.OnStartRoutine(It.IsAny<RunFixedLenghtRoutinePacket>()));
+
+
+        overlayDispatcher.RegisterHandler(mockHandler.Object);
+
+        mockConnection.Raise(client => client.DataReceived += null, JsonDocument.Parse(serializedJson));
+
+        overlayDispatcher.Dispose();
+
+        mockHandler.Verify(
+            adapter => adapter.OnStartRoutine(
+                It.Is<RunFixedLenghtRoutinePacket>(p => p.RoutineName == packet.PacketData.RoutineName)), Times.Once);
+    }
+
+    private class LoggerImpl : ILogger
+    {
+        private Microsoft.Extensions.Logging.ILogger _logger;
+
+        public LoggerImpl(Microsoft.Extensions.Logging.ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Debug(string message)
+        {
+            _logger.LogDebug(message);
+        }
+
+        public void Info(string message)
+        {
+            _logger.LogInformation(message);
+        }
+
+        public void Warn(string message)
+        {
+            _logger.LogWarning(message);
+        }
+
+        public void Error(string message, Exception? ex = null)
+        {
+            _logger.LogError(message + ": {}", ex?.Message);
+        }
+    }
+
+    [Fact]
+    public async Task SimulatedIntegrationTest()
+    {
+        ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole().AddDebug());
+        var logger1 = factory.CreateLogger("Dispatcher1");
+        var logger2 = factory.CreateLogger("Dispatcher2");
+        LoggerImpl loggerImpl1 = new LoggerImpl(logger1);
+        LoggerImpl loggerImpl2 = new LoggerImpl(logger2);
+
+
+        RunFixedLenghtRoutinePacket packet = new RunFixedLenghtRoutinePacket("balls");
+
+
+        var isFinishedReading = new TaskCompletionSource();
+        var mockHandler1 = new Mock<PacketHandlerAdapter>();
+        mockHandler1
+            .Setup(adapter => adapter.OnTermination())
+            .Callback(() => { isFinishedReading.SetResult(); });
+        mockHandler1
+            .Setup(adapter => adapter.OnStartRoutine(It.IsAny<RunFixedLenghtRoutinePacket>()));
+
+        var task1 = Task.Run(async () =>
+        {
+            SocketFactory sfactory = new SocketFactory();
+            var sock = sfactory.CreateClient("127.0.0.1", 1234);
+            EventDrivenTcpClient tcp = new EventDrivenTcpClient(sock);
+            EventDrivenJsonClient client = new EventDrivenJsonClient(tcp);
+            OverlayMessageDispatcher messageDispatcher = new OverlayMessageDispatcher(loggerImpl1, client);
+            messageDispatcher.Dispatch(packet);
+            messageDispatcher.Dispatch(new EndOfConnectionPacket());
+
+            await isFinishedReading.Task;
+            Assert.False(messageDispatcher.IsConnected());
+        });
+        var task2 = Task.Run(async () =>
+        {
+            SocketFactory sfactory = new SocketFactory();
+            var sock = sfactory.CreateServer("127.0.0.1", 1234);
+            EventDrivenTcpClient tcp = new EventDrivenTcpClient(sock);
+            EventDrivenJsonClient client = new EventDrivenJsonClient(tcp);
+            OverlayMessageDispatcher messageDispatcher = new OverlayMessageDispatcher(loggerImpl2, client);
+            messageDispatcher.RegisterHandler(mockHandler1.Object);
+
+            await isFinishedReading.Task;
+            Assert.False(messageDispatcher.IsConnected());
+        });
+
+
+        await task1;
+        await task2;
+
+        mockHandler1.Verify(
+            adapter => adapter.OnStartRoutine(
+                It.Is<RunFixedLenghtRoutinePacket>(p => p.RoutineName == packet.RoutineName)), Times.Once);
+    }
+}
