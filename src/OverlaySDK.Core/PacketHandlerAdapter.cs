f20eeb413eae0dca81971ccc385d1c6ab906def1
RedInJector <imaxprots@gmail.com>
2025-10-02 01:30:24 +0200

    fixups

--- src/OverlaySDK.Core/PacketHandlerAdapter.cs
+++ src/OverlaySDK.Core/PacketHandlerAdapter.cs
@@ -3,23 +3,16 @@
 namespace OverlaySDK;
 
 public abstract class PacketHandlerAdapter
+{
+    public virtual void OnStartRoutine(RunFixedLenghtRoutinePacket routine)
     {
-        public virtual void OnStartRoutine(RunFixedLenghtRoutinePacket routine)
-        {
-
-        }
-
-        public virtual void OnEOC(EndOfConnectionPacket eoc)
-        {
-
-        }
-
-        public virtual void OnTermination()
-        {
-
-        }
-        public virtual void OnException(Exception ex)
-        {
-
-        }
     }
+
+    public virtual void OnStartRoutine(RunVariableLenghtRoutinePacket routine)
+    {
+    }
+
+    public virtual void OnTermination()
+    {
+    }
+}
--- src/OverlaySDK.Core/Packets/Packets.cs
+++ src/OverlaySDK.Core/Packets/Packets.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Text.Json;
+﻿using System.Text.Json;
 
 namespace OverlaySDK.Packets;
 
@@ -8,7 +7,10 @@ public interface IPacket;
 public record InitializePacket(string AppVersion) : IPacket
 {
 }
-public record EndOfConnectionPacket() : IPacket{}
+
+public record EndOfConnectionPacket() : IPacket
+{
+}
 
 public record RunFixedLenghtRoutinePacket(string RoutineName) : IPacket
 {
@@ -18,19 +20,19 @@ public record RunVariableLenghtRoutinePacket(string RoutineName, TimeSpan Time) 
 {
 }
 
-public class HmdPositionalDataPacket {
-
-    public float RoutinePitch { get; set; }        // degrees
-    public float RoutineYaw { get; set; }          // degrees
-    public float RoutineDistance { get; set; }     // meters
-    public float RoutineConvergence { get; set; }  // 0..1
-    public float FovAdjustDistance { get; set; }   // units
+public class HmdPositionalDataPacket : IPacket
+{
+    public float RoutinePitch { get; set; } // degrees
+    public float RoutineYaw { get; set; } // degrees
+    public float RoutineDistance { get; set; } // meters
+    public float RoutineConvergence { get; set; } // 0..1
+    public float FovAdjustDistance { get; set; } // units
 
     // Per-eye gaze
-    public float LeftEyePitch { get; set; }        // degrees
-    public float LeftEyeYaw { get; set; }          // degrees
-    public float RightEyePitch { get; set; }       // degrees
-    public float RightEyeYaw { get; set; }         // degrees
+    public float LeftEyePitch { get; set; } // degrees
+    public float LeftEyeYaw { get; set; } // degrees
+    public float RightEyePitch { get; set; } // degrees
+    public float RightEyeYaw { get; set; } // degrees
 }
 
 public class Packet<T> where T : IPacket
@@ -56,4 +58,3 @@ public class IncomingPacket
         PacketData = packetData;
     }
 }
-
